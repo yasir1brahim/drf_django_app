4d29aff2d251e1627f116daf94730b1eadbba590
HasanQadir <hqadir@knysys.com>
2021-09-02 09:03:59 +0500

    Remove redundant line spaces

--- drf_app/test_views.py
+++ drf_app/test_views.py
@@ -15,7 +15,6 @@ class UserViewCase(TestCase):
     def test_get(self):
         status_code = 200
         view_class = views.UserView
-
         req = self.factory.get('/api/user/')
         resp = view_class.as_view()(req, *[], **{})
         self.assertEqual(resp.status_code, status_code)
@@ -23,7 +22,6 @@ class UserViewCase(TestCase):
     def test_post(self):
         status_code = 201
         view_class = views.UserView
-
         data = {'first_name':'philipp', 'last_name':'hafner'}
         req = self.factory.post('/api/user/', data, content_type='application/json')
         resp = view_class.as_view()(req, *[], **{})
@@ -32,7 +30,6 @@ class UserViewCase(TestCase):
     def test_put(self):
         status_code = 200
         view_class = views.UserView
-
         data = {'first_name': 'philipp', 'last_name': 'hazner'}
         req = self.factory.put(
             '/api/user/?pk=' + str(self.user.id), data=data, content_type='application/json')
@@ -42,7 +39,6 @@ class UserViewCase(TestCase):
     def test_delete(self):
         status_code = 204
         view_class = views.UserView
-
         req = self.factory.delete(
             '/api/user/?pk=' + str(self.user.id))
         resp = view_class.as_view()(req, *[], **{})
@@ -72,7 +68,6 @@ class SeminarViewCase(TestCase):
     def test_post(self):
         status_code = 201
         view_class = views.SeminarView
-
         data = {'title':'Computer Information',
                 'description':'Computer Information and their impact in our society',
                 'users': list(str(self.user.id))}
@@ -83,7 +78,6 @@ class SeminarViewCase(TestCase):
     def test_put(self):
         status_code = 200
         view_class = views.SeminarView
-
         data = {'title': 'Computer Information',
                 'description': 'Computer Information and their impact in our society',
                 'users': list(str(self.user.id))}
@@ -95,7 +89,6 @@ class SeminarViewCase(TestCase):
     def test_delete(self):
         status_code = 204
         view_class = views.SeminarView
-
         req = self.factory.delete(
             '/api/seminar/?pk=' + str(self.seminar.id))
         resp = view_class.as_view()(req, *[], **{})
